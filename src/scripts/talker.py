ac32ffb86902a145f716906180c2f7e033d30341
Nikita Loabzev <pavilich99@mail.ru>
2022-10-12 14:11:41 +0300

    Update talker.py

--- src/scripts/talker.py
+++ src/scripts/talker.py
@@ -4,12 +4,14 @@ from std_msgs.msg import String
 
 rospy.init_node('talker')
 pub = rospy.Publisher('my_chat_topic', String, queue_size=10)
-rate = rospy.Rate(1)
+rate = rospy.Rate(100)
 
 def start_talker():
     msg = String()
+    i = 0
     while not rospy.is_shutdown():
-        hello_str = "hi =)"
+        hello_str = str(i)
+        i += 2
         rospy.loginfo(hello_str)
 
         msg.data = hello_str
