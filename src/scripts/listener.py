1d6b30afbd89b0e454a1a58d7025515b883dd9f2
Nikita Loabzev <pavilich99@mail.ru>
2022-10-29 07:48:49 +0300

    f

--- src/scripts/listener.py
+++ src/scripts/listener.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
+# необходимо для запуска, она сообщает системе о том,
+# что данный файл необходимо запускать через интерпретатор `python`
 
+import rospy # импортируем основной модуль `rospy`
+from std_msgs.msg import String # импортируем сообщения типа String
+
+# далее пишем обработчик приема сообщений из топика,
+# регистрируем узел и подписываемся на топик с указанием обработчика.
+# обработчик вызывается каждый раз, как узел получает сообщение.
 def callback(msg):
     rospy.loginfo("I heard %s", msg.data)
 
 rospy.init_node('listener')
-rospy.Subscriber('my_chat_topic', String, callback, queue_size=10)
+rospy.Subscriber('my_chat_topic', String, callback, queue_size=10) # не требуется сохранять объект подписки, возврат функции игнорируется
 rospy.spin()
--- src/scripts/talker.py
+++ src/scripts/talker.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
+# необходимо для запуска, она сообщает системе о том,
+# что данный файл необходимо запускать через интерпретатор `python`
 
-rospy.init_node('talker')
+import rospy # импортируем основной модуль `rospy`
+from std_msgs.msg import String # импортируем сообщения типа String
+
+rospy.init_node('talker') # задает название, которое будет зарегистрировано в рабочей экосистеме ROS
 pub = rospy.Publisher('my_chat_topic', String, queue_size=10)
-rate = rospy.Rate(1)
+rate = rospy.Rate(1) # используется для выдерживания частоты выполнения кода, Гц
 
 def start_talker():
-    msg = String()
+    msg = String() # создаем объект сообщения
     i = 0
-    while not rospy.is_shutdown():
-        hello_str = str(i)
+    while not rospy.is_shutdown(): # бесконечный цикл, пока ROS система работает
+        hello_str = str(i) # формируем сообщение
         i += 2
-        rospy.loginfo(hello_str)
+        rospy.loginfo(hello_str)  # вывод в терминал информации (содержание сообщения)
 
-        msg.data = hello_str
-        pub.publish(msg)
+        msg.data = hello_str # заполнение сообщения
+        pub.publish(msg) # публикация сообщения в топик
 
-        rate.sleep()
+        rate.sleep() # сон в соответствии с выдерживаемой частотой
 
 try:
     start_talker()
