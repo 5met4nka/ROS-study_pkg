e434bfb4d9411ad96a9d19afcc86d16a7369f227
Nikita Loabzev <pavilich99@mail.ru>
2022-11-11 16:43:30 +0300

    Update turtle_tf_broadcaster.py

--- src/scripts/turtle_tf_broadcaster.py
+++ src/scripts/turtle_tf_broadcaster.py
@@ -3,24 +3,19 @@
 import rospy # импорт основного модуля
 import tf # импорт модуля TF
 import math
+import time
 from tf.transformations import quaternion_from_euler # импорт, который исключает необходимость прописывания `tf.transformations.quaternion_from_euler()`
 from turtlesim.msg import Pose # импорт модуля содержащий типы сообщений из пакета
 
 def handle_turtle_and_carrot_pose(msg):
 
-    angle = 0
-
+    angle = int((time.time() * 100)) % 360
+    print(angle)
+    x = 1 * math.cos(angle * 0.0174533)
+    y = 1 * math.sin(angle * 0.0174533)
     br = tf.TransformBroadcaster() # получаем объект публикатора информации TF
     br.sendTransform((msg.x, msg.y, 0), quaternion_from_euler(0, 0, msg.theta), rospy.Time.now(), turtlename, "world")
-    br.sendTransform((2, 2, 0), quaternion_from_euler(0, 0, 0), rospy.Time.now(), "carrot", "turtle1")
-    # while True:
-    #     if angle >= 360:
-    #         angle = 0
-    #     x = 1*math.cos(angle*0.0174533)
-    #     y = 1*math.sin(angle*0.0174533)
-    #     br.sendTransform((msg.x, msg.y, 0), quaternion_from_euler(0, 0, msg.theta), rospy.Time.now(), turtlename, "world")
-    #     br.sendTransform((x, y, 0), quaternion_from_euler(0, 0, 0), rospy.Time.now(), "carrot", "turtle1")
-    #     angle += 1
+    br.sendTransform((x, y, 0), quaternion_from_euler(0, 0, 0), rospy.Time.now(), "carrot", "turtle1")
 
 if __name__ == '__main__':
 
